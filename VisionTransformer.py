4a92665d9721e4f570271e5ee4dc904c9b5f7320
lukabarbakadze <luka.barbaqadze38@gmail.com>
2023-05-29 11:45:02 +0400

    upload project files

--- VisionTransformer.py
+++ VisionTransformer.py
@@ -2,18 +2,14 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-n_patches = 7
-num_heads = 4
-head_size = 16
-dropout = 0.2
-num_encoder_block = 4
 ######################################### Divide Image to Patches ############################################
-class ImagePreprocesser:
+class PatchMaker(nn.Module):
     """
     --input
-    n_patches: number of patches (if we want x^2 cube patches from image, n_patch=x)
+    n_patches: number of patches (if we want x^2 patches from image, n_patch=x),
     """
     def __init__(self, n_patches):
+        super().__init__()
         self.n_patches = n_patches
     
     @staticmethod
@@ -31,7 +27,7 @@ class ImagePreprocesser:
                     patches[idx, i * n_patches + j] = patch.flatten()
         return patches
     
-    def __call__(self, x):
+    def forward(self, x):
         """
         --input
         x:       batch of images (batch_size, C, H, W)
@@ -39,24 +35,23 @@ class ImagePreprocesser:
         patches: batch of patches (batch_size, n_patches*n_patches, patch_size*patch_size)
         """
         return self.patchify(x, self.n_patches)
-
-######################################### Single Head Attention ############################################## 
+######################################### Single Head Attention ##############################################
 class Head(nn.Module):
     def __init__(self, head_size, d_emb, dropout=0.2):
         """
         --input
         head_size: maximum context length,
         d_emb:     dimension of each flattened patch (if size of patch is (c,x,x), d_embd=c*x*x),
-        dropout:   p for dropout
+        dropout:   probability for dropout layer
         --output   (forward method)
         out:        self-attention weights of x
         """
         super().__init__()
         self.d_embed = d_emb # 20
         self.head_size = head_size # 16
-        self.q = nn.Linear(d_emb, head_size, bias=False) # weight dims: (head_size, d_emb)
-        self.k = nn.Linear(d_emb, head_size, bias=False) # weight dims: (head_size, d_emb)
-        self.v = nn.Linear(d_emb, head_size, bias=False) # weight dims: (head_size, d_emb)
+        self.q = nn.Linear(d_emb, head_size, bias=True) # weight dims: (head_size, d_emb)
+        self.k = nn.Linear(d_emb, head_size, bias=True) # weight dims: (head_size, d_emb)
+        self.v = nn.Linear(d_emb, head_size, bias=True) # weight dims: (head_size, d_emb)
         self.dropout = nn.Dropout(dropout)
         
     def forward(self, x):
@@ -64,7 +59,7 @@ class Head(nn.Module):
         key   = self.k(x) # (B,T,head_size)
         value = self.v(x) # (B,T,head_size)
         
-        wei = torch.matmul(query, key.transpose(-1,-2)) * self.head_size**-0.5 # (B,T,head_size) @ (B,head_size,T) -> (B,T,T)
+        wei = torch.matmul(query, key.transpose(-1,-2)) / (self.head_size**0.5) # (B,T,head_size) @ (B,head_size,T) -> (B,T,T)
         wei = F.softmax(wei, dim=-1)
         wei = self.dropout(wei)
         
@@ -78,18 +73,17 @@ class MultiHeadAttention(nn.Module):
         num_heads: number of heads,
         head_size: maximum context length,
         d_emb:     dimension of each flattened patch (if size of patch is (c,x,x), d_embd=c*x*x),
-        dropout:   p for dropout
+        dropout:   probability for dropout layer
         --output
         out:       multi-head attention weights
         """
         super().__init__()
-        self.heads = nn.ModuleList([Head(head_size, d_emb) for _ in range(num_heads)]) # (B,T,head_size)
+        self.heads = nn.ModuleList([Head(head_size, d_emb, dropout=dropout) for _ in range(num_heads)]) # (B,T,head_size)
         self.proj = nn.Linear(head_size * num_heads, d_emb) # (B,T,d_emb)
-        self.dropout = nn.Dropout(dropout)
 
     def forward(self, x):
         out = torch.cat([h(x) for h in self.heads], dim=-1)
-        out = self.dropout(self.proj(out))
+        out = self.proj(out)
         return out
 ######################################### FeedFowrard Layer ##################################################
 class FeedFoward(nn.Module):
@@ -97,28 +91,28 @@ class FeedFoward(nn.Module):
         """
         --input
         d_emb:   dimension of each flattened patch (if size of patch is (c,x,x), d_embd=c*x*x),
-        dropout: p for dropout
+        dropout: probability for dropout layer
         """
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(d_emb, 4 * d_emb),
-            nn.GELU(),
+            nn.LeakyReLU(),
             nn.Linear(4 * d_emb, d_emb),
             nn.Dropout(dropout)
-        )
+            )
 
     def forward(self, x):
         return self.net(x) # (B,T,d_emb)
 
 ######################################### Encoder Block ######################################################
 class Block(nn.Module):
-    def __init__(self, head_size, d_emb, num_heads, dropout):
+    def __init__(self, head_size, d_emb, num_heads, dropout=0.2):
         """
         --input
         num_heads: number of heads,
         head_size: maximum context length,
         d_emb:     dimension of each flattened patch (if size of patch is (c,x,x), d_embd=c*x*x),
-        dropout:   p for dropout
+        dropout:   probability for dropout layer
         """
         super().__init__()
         self.sa = MultiHeadAttention(num_heads, head_size, d_emb, dropout=dropout)
@@ -138,7 +132,7 @@ class VisionTransformer(nn.Module):
                  num_heads,
                  head_size,
                  n_classes,
-                 hidden_dim=16, 
+                 hidden_dim=4, 
                  input_dims=(1,28,28), 
                  n_patches=7,
                  dropout=0.2):
@@ -151,7 +145,7 @@ class VisionTransformer(nn.Module):
         hidden_dim:        hidden layer dimension
         input_dims:        dimension if images
         n_patches:         number of patches for each image
-        dropout:           p for dropout
+        dropout:           probability for dropout layer
         --output
         x:                 raw logits (whithout softmax)
         """
@@ -165,7 +159,7 @@ class VisionTransformer(nn.Module):
         self.input_d = int(input_dims[0] * self.patch_size[0] * self.patch_size[1])
 
         # define linear projection layer for input
-        self.linear_mapper = nn.Linear(self.input_d, self.hidden_dim, bias=False)
+        self.linear_mapper = nn.Linear(self.input_d, self.hidden_dim)
 
         # prepare positional encoding layer
         self.pos_embd = nn.Embedding(n_patches * n_patches + 1, hidden_dim)
@@ -177,7 +171,7 @@ class VisionTransformer(nn.Module):
         self.register_buffer('positional_embedding', torch.arange(n_patches * n_patches + 1))
 
         # define image preprocessor (slices batch of images to batch of patches)
-        self.patchify = ImagePreprocesser(n_patches=n_patches)
+        self.patchify = PatchMaker(n_patches=n_patches)
 
         # define number of transformer blocks
         self.blocks = nn.Sequential(*[Block(head_size=head_size, d_emb=hidden_dim, num_heads=num_heads, dropout=dropout) for _ in range(num_encoder_block)])
